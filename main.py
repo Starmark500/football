d4b298a70c5d0750fa92488916fd40f79529470f
stepz <step.zip@mail.ru>
2023-11-24 18:10:51 +0300

    platform work

--- main.py
+++ main.py
@@ -10,20 +10,47 @@ platform2 = sprite.add("mario-items", 150, 250, "moving_platform3")
 sprite.set_angle(platform2, 180)
 sprite.set_angle(platform1, 180)
 speed_x=2
-speed_y=0
+speed_y=2
 right_ball=sprite.get_right(ball)
 
-top_ball=sprite.get_top(ball)
-bottom_ball=sprite.get_bottom(ball)
+
 @wrap.always(10)
 def fly_ball():
-    global speed_x
+    global speed_x,speed_y
     sprite.move(ball, speed_x, speed_y)
     right_ball = sprite.get_right(ball)
     left_ball = sprite.get_left(ball)
-    print(right_ball)
+    top_ball = sprite.get_top(ball)
+    bottom_ball = sprite.get_bottom(ball)
+    print(bottom_ball)
     if right_ball > 1000:
-        speed_x=-2
+        speed_x=-speed_x
+    if left_ball < 0:
+        speed_x=-speed_x
+    if bottom_ball > 500:
+        speed_y=-speed_y
+    if top_ball < 0:
+        speed_y=-speed_y
+    otbivka_platform()
+@wrap.on_key_always(wrap.K_DOWN,wrap.K_UP)
+def move_platform(keys):
+    if wrap.K_DOWN in keys:
+        sprite.move(platform1,0,10)
+    else:
+        sprite.move(platform1,0,-10)
+@wrap.on_key_always(wrap.K_w,wrap.K_s)
+def move_platform(keys):
+    if wrap.K_s in keys:
+        sprite.move(platform2,0,10)
+    else:
+        sprite.move(platform2,0,-10)
+
+def otbivka_platform():
+    global speed_x
+    left_platform=sprite.get_left(platform1)
+    if sprite.is_collide_sprite(ball,platform1):
+        sprite.move_right_to(ball,left_platform)
+        speed_x=-speed_x
 
 
 
